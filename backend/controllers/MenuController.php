f011d7231507e2ff470f822c2b094e7deeebc642
Coderyounes <younesanas2021@gmail.com>
2024-12-19 22:21:22 +0100

    FEAT: Itegration d'une function pour ajouter des Plat sur un Menu #32

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -2,11 +2,39 @@
 
 class MenuController{
     
+    // Menu Will be Based on Plats
     public static function getMenu() {}
     public static function getAllMenus() {}
     public static function updateMenu() {}
     public static function deleteMenu() {}
-    public static function createMenu() {}
+    public static function createMenu() {
+        // in FrontEnd just USE AJAX to fetch the Plats & It ID 
+        // then Pass them Back with the Selected Choices
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $user = AuthController::ValidateUser();
+
+        $db = DBconnection::getConnection()->connection;
+
+        $insertMenu = "INSERT INTO Menu (Nom, Created_by) VALUES(?, ?)";
+        $stmt = $db->prepare($insertMenu);
+        $stmt->bind_param('si', $data['Nom'], $user['ID']);
+        $stmt->execute();
+
+        $lastID = $db->insert_id;
+
+        $stmt->close();
+        $PlatsMenu = "INSERT INTO Menu_Plats (Menu_id, Plat_id) VALUES (?, ?)";
+        $stat = $db->prepare($PlatsMenu);
+        $stat->bind_param("ii", $lastID, $data['Plat_id']);
+        if($stat->execute()) {
+            $stat->close();
+            return 1;
+        }
+
+        $stat->close();
+        return -1;
+    }
 }
 
 ?>
--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -5,6 +5,7 @@ class UserController
 {
     public static function createReservation() {}
     public static function getReservation() {}
+    public static function getOneReservation() {}
     public static function updateReservation() {}
     public static function removeReservation() {}
     
